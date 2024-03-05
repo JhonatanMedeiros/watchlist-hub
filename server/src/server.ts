aa9edbf868c0e2160ac1816b815dc06002142e49
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2024-03-04 21:36:20 -0300

    ✨ feat: Initial server-side project

---
+++ server/src/server.ts
@@ -0,0 +1,8 @@
+import fastify from 'fastify'
+const app = fastify()
+
+app.get('/', async () => {
+  return { hello: 'world' }
+})
+
+app.listen({ port: 3333 }).then(() => { console.log('Server running on http://localhost:3333') })
